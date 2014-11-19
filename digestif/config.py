6241a7ab1000e4334525e3e9fac4ecccaecffb38
James Clarke <james@jamesclarke.net>
2014-11-19 02:10:48 +0000

    Add OpenGraph, Twitter, Facebook support
    Use OpenGraph for displaying digests on Facebook and Twitter.

--- digestif/config.py
+++ digestif/config.py
@@ -5,7 +5,7 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/Jclarke/tmp/test.db'
     SECRET_KEY = keys.FLASK_SECRET
-    
+    FB_APP_ID = keys.FB_APP_ID
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@localhost/digestif".format(keys.DB_USERNAME, keys.DB_PASSWORD)
