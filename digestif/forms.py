aa27209f0a4e0c9e61f390caff33e638f5970294
James Clarke <james@zaius.com>
2016-08-09 12:30:00 -0400

    Remove instagram sign-up

--- digestif/forms.py
+++ digestif/forms.py
@@ -3,9 +3,8 @@ from wtforms import TextField, validators, HiddenField, SelectField
 
 class SignUpStream(Form):
     email = TextField("Email address", [validators.Email("Please enter a valid email address.")])
-    stream = HiddenField("stream", [validators.AnyOf(["flickr", "instagram"], message="You forgot to click the Flickr or Instagram icon.")])
+    stream = HiddenField("stream", [validators.AnyOf(["flickr", "instagram"], message="You forgot to click the Flickr or Instagram icon.")], default="flickr")
 
 class SubscribeForm(Form):
     email = TextField("Email", [validators.Email(message="Please enter a valid email address")])
-    frequency = HiddenField("frequency", [validators.AnyOf(["0", "1", "2", "3", "7", "14"])]) 
-
+    frequency = HiddenField("frequency", [validators.AnyOf(["0", "1", "2", "3", "7", "14"])])
