7e1c24112de264d1f872c70e381e5fc852290365
James Clarke <james@zaius.com>
2016-08-10 15:24:45 -0400

    Basic environment uses mysql

--- digestif/config.py
+++ digestif/config.py
@@ -3,7 +3,8 @@ import keys
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/Jclarke/tmp/test.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/Jclarke/tmp/test.db'
+    SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@localhost/digestif".format(keys.DB_USERNAME, keys.DB_PASSWORD)
     SECRET_KEY = keys.FLASK_SECRET
     FB_APP_ID = keys.FB_APP_ID
 
